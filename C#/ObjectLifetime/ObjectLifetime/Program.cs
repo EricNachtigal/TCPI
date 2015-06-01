9a13aab0a45daa93cb8436bf2f95291d3aed391c
EricNachtigal <EricNachtigal@gmail.com>
2015-06-01 15:29:30 -0700

    Lesson 17 halfway through

---
+++ C#/ObjectLifetime/ObjectLifetime/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectLifetime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car myCar = new Car();
+
+            // Set properties
+
+            Car myOtherCar = myCar;
+
+            Car myThirdCar = new Car("Ford", "Escape", 2005, "Red");
+
+            myOtherCar = null;
+            myCar = null;
+
+            Car.
+
+        }
+    }
+
+    class Car
+    {
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public string Color { get; set; }
+        public double OriginalPrice { get; set; }
+
+        public Car()
+        {
+            //You could load this from a configuration file, a database, etc.
+            //We will just hardcode it in this instance
+            this.Make = "Nissan";
+        }
+
+        public Car(string make, string model, int year, string color)
+        {
+            Make = make;
+            Model = model;
+            Year = year;
+            Color = color;
+        }
+        /*
+        public Car(string someOtherInputParameter, string model, int year, string color)
+        {
+            Make = someOtherInputParameter;
+            Model = model;
+            Year = year;
+            Color = color;
+        }
+        */
+
+        public static void MyMethod()
+        {
+
+        }
+    }
+
+}
---
+++ C#/UnderstandingInheritance/UnderstandingInheritance/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnderstandingInheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car myCar = new Car();
+            myCar.Make = "BMW";
+            myCar.Model = "745li";
+            myCar.Color = "Black";
+            myCar.Year = 2005;
+
+            printCarDetails(myCar);
+
+            Truck myTruck = new Truck();
+            myTruck.Make = "Ford";
+            myTruck.Model = "F350";
+            myTruck.Color = "Red";
+            myTruck.Year = 2006;
+            myTruck.TowingCapacity = 1200;
+            printCarDetails(myTruck);
+            
+            Console.ReadLine();
+        }
+
+        private static void printCarDetails(Car car)
+        {
+             Console.WriteLine("Here are the Car's details: {0}",
+             car.FormatMe());
+        }
+
+    }
+
+    class Car
+    {
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public string Color { get; set; }
+
+        public string FormatMe()
+        {
+            return String.Format("{0} - {1} - {2} - {3}",
+                this.Make,
+                this.Model,
+                this.Color,
+                this.Year);
+        }
+    }
+
+    class Truck : Car
+    {
+        public int TowingCapacity { get; set; }
+
+        public override string FormatMe()
+        {
+            return String.Format("{0} - {1} - {2} lbs",
+                this.Make,
+                this.Model,
+                this.TowingCapacity);
+        }
+    }
+
+}
